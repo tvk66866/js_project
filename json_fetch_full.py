6b6afaa83b0cba31e6ce396e518e96781c042f5f
vasanththirugnanam <tvk66866@gmail.com>
2020-05-06 16:16:30 -0700

    update to html and model parameter

--- json_fetch_full.py
+++ json_fetch_full.py
@@ -41,6 +41,34 @@ def worker():
 	# 	result = item['blood_pressure']
 	# print(result)	
 
+	# # start
+	# obs_data = pd.read_excel('fhir_Observation_data.xlsx')
+	# obs_transform = obs_data.pivot_table(values = 'obs_value', index = 'patient_name', columns='obs_name',aggfunc='mean',)
+	# obs_transform.columns = obs_transform.columns.str.replace('.','_').str.replace("/","_")
+	# select_col = obs_transform.notnull().sum().sort_values(ascending=False)[0:11].index
+	#
+	# select_obs = obs_transform[select_col]
+	# model = XGBRegressor(booster='gbtree',objective='reg:squarederror',larning_rate = 0.0001, \
+	# 					 max_depth = 2, min_child_weight = 1, n_estimators = 50, subsample = 0.4)
+	# X = select_obs.drop(['Body Weight','Body Mass Index'], axis=1)
+	# y = select_obs['Body Weight']
+	# y.replace(np.NaN,y.mean(),inplace=True)
+	# model.fit(X, y)
+	# # end
+	print(model)
+	prod_ds = pd.DataFrame(data)
+	prod_col = prod_ds.columns
+	for col in prod_col:
+   	    prod_ds[col] = prod_ds[col].astype(float)
+	y_pred = model.predict(prod_ds)
+	print(y_pred[0])
+	return str(round(y_pred[0],2))
+
+
+
+if __name__ == '__main__':
+	# run!
+	print('In Main fuction')
 	# start
 	obs_data = pd.read_excel('fhir_Observation_data.xlsx')
 	obs_transform = obs_data.pivot_table(values = 'obs_value', index = 'patient_name', columns='obs_name',aggfunc='mean',)
@@ -55,17 +83,4 @@ def worker():
 	y.replace(np.NaN,y.mean(),inplace=True)
 	model.fit(X, y)
 	# end
-	prod_ds = pd.DataFrame(data)
-	prod_col = prod_ds.columns
-	for col in prod_col:
-   	    prod_ds[col] = prod_ds[col].astype(float)
-	y_pred = model.predict(prod_ds)
-	print(y_pred[0])
-	return str(round(y_pred[0],2))
-
-
-
-if __name__ == '__main__':
-	# run!
-	print('In Main fuction')
 	app.run()
